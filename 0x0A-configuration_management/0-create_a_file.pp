5032f17a64f5f7f63d971d17e3a05516faf7fc2e
DanielaIH <4427@holbertonstudents.com>
2022-08-22 18:49:58 -0700

    ADD README.md 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-node default {
+# Using Puppet, create a file in /tmp.
 file { '/tmp/school':
-ensure  => 'present',
 content => 'I love Puppet',
 owner   => 'www-data',
 group   => 'www-data',
 mode    => '0744',
-}
 }
